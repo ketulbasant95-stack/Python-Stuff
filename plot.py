347569a03f0dfba9ccfcafd0740e02b7179dc324
Ketul Basant <ketulbasant95@gmail.com>
2026-02-18 06:51:55 +0530

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,193 @@
+import matplotlib.pyplot as plt
+
+#Line Graph
+
+def linegraph(x, y):
+    "Used to plot a line graph in the Cartesian plane."
+
+    numx = int(input("Enter number of values of X-axis: "))
+    numy = int(input("Enter number of values of Y-axis: "))
+    for i in range(numx):
+        x = eval(input("Enter the values of x-axis (one by one): "))
+        xval = [x]
+        print("Number of values left:", numx-i)
+    for j in range(numy):
+        y = eval(input("Enter the values of y-axis (one by one): "))
+        yval = [y]
+        print("Number of values left:", numy-j)
+
+    xaxis = input("Define X-axis (optional): ")
+    yaxis = input("Define Y-axis (optional): ")
+    title = input("Enter a title for the graph (optional): ")
+    if xaxis=="":
+        xaxis = "X-axis"
+    if yaxis=="":
+        yaxis = "Y-axis"
+    if title=="":
+        title = "Graph"
+
+    null = 0
+    ch = input("Would you like to modify the graph appearance? (y/n): ")
+    if ch=="y":
+        null = 1
+        color = input("Enter the color of line: ")
+        style = input("Enter the line style: ")
+        width = int(input("Enter the line width: "))
+    else:
+        pass
+
+    opt = int(input("Would you like to review your data? (y/n): "))
+    if opt=="y":
+        print(xval)
+        print(yval)
+    else:
+        pass
+
+    if null==0:
+        plt.plot(xval, yval)
+        plt.title(title)
+        plt.ylabel(yaxis)
+        plt.xlabel(xaxis)
+        plt.show()
+    else:
+        plt.plot(x, y, color = color, style = style, width = width)
+        plt.title(title)
+        plt.ylabel(yaxis)
+        plt.xlabel(xaxis)
+        plt.show()
+
+#Bar graph
+
+def bargraph(x, y):
+    "Used to plot bar graph with given data."
+
+    numx = int(input("Enter the number of values for X-axis: "))
+    numy = int(input("Enter the number of values for Y-axis: "))
+    for i in range(numx):
+        x = eval(input("Enter the value of X-axis:"))
+        xval = [x]
+        print("Number of entries remaining:", numx-i)
+    for j in range(numy):
+        y = eval(input("Enter the value of X-axis: "))
+        yval = [y]
+        print("Number of entries remaining:", numy-j)
+    
+    xaxis = input("Define X-axis (optional): ")
+    yaxis = input("Define Y-axis (optional): ")
+    title = input("Enter a title for the graph (optional): ")
+    if xaxis=="":
+        xaxis = "X-axis"
+    if yaxis=="":
+        yaxis = "Y-axis"
+    if title=="":
+        title = "Graph"
+
+    opt = int(input("Would you like to review your data? (y/n): "))
+    if opt=="y":
+        print(xval)
+        print(yval)
+    else:
+        pass
+
+    plt.plot(x, y)
+    plt.title(title)
+    plt.ylabel(yaxis)
+    plt.xlabel(xaxis)
+    plt.show()
+
+#Histogram
+
+def histogram(x, bins):
+    ""
+    numx = int(input("Enter the number of values of X-axis: "))
+    bins = int(input("Enter bins: "))
+    
+    for i in range(numx):
+        x = int(input("Enter the value of X-axis:"))
+        xval = [x]
+        print("Number of entries remaining:", numx-i)
+        
+        xaxis = input("Define X-axis (optional): ")
+        yaxis = input("Define Y-axis (optional): ")
+    title = input("Enter a title for the graph (optional): ")
+    if xaxis=="":
+        xaxis = "X-axis"
+    if yaxis=="":
+        yaxis = "Y-axis"
+    if title=="":
+        title = "Graph"
+
+    opt = int(input("Would you like to review your data? (y/n): "))
+    if opt=="y":
+        print(xval)
+    else:
+        pass
+
+    plt.hist(x, bins)
+    plt.title(title)
+    plt.xlabel(xaxis)
+    plt.ylabel(yaxis)
+    plt.show()
+
+#Scatter plot
+
+def scatter(x, y):
+    "Used to observe relationships between variables visually."
+
+    numx = int(input("Enter the number of values of X-axis: "))
+    numy = int(input("Enter the number of values of Y-axis: "))
+    
+    for i in range(numx):
+        x = int(input("Enter the value of X-axis:"))
+        xval = [x]
+        print("Number of entries remaining:", numx-i)
+    for j in range(numy):
+        y = int(input("Enter the vale of Y-axis: "))
+        yval = [y]
+        print("Number of entries remaining:", numy-j)
+
+    xaxis = input("Define X-axis (optional): ")
+    yaxis = input("Define Y-axis (optional): ")
+    title = input("Enter a title for the graph (optional): ")
+    if xaxis=="":
+        xaxis = "X-axis"
+    if yaxis=="":
+        yaxis = "Y-axis"
+    if title=="":
+        title = "Graph"
+
+    opt = int(input("Would you like to review your data? (y/n): "))
+    if opt=="y":
+        print(xval)
+    else:
+        pass
+
+    plt.scatter(xval, yval)
+    plt.title(title)
+    plt.xlabel(xaxis)
+    plt.ylabel(yaxis)
+    plt.show()
+
+#Pie Graph
+
+def pie(data, label):
+    "Used to visually compare data proportions."
+
+    numx = int(input("Enter the number of labels: "))
+
+    for i in range(numx):
+        x = input("Enter the label: ")
+        label = [x]
+        print("Number of entries remaining: ", numx-i)
+    
+    for j in range(0, len(label)):
+        y = int(input("Enter the value for", label[j], ":"))
+        data = [y]
+
+    title = input("Enter title (optional): ")
+    if title=="":
+        title = "Pie Chart"
+    
+    plt.pie(data, label)
+    plt.title(title)
+    plt.show()
